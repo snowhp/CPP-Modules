88d27750815b755a18a7bdc179bbf1599f99626a
SnowHP <13867481+snowhp@users.noreply.github.com>
2023-10-25 12:43:41 +0100

    Add: First phonebook version

---
+++ CPP00/ex01/header.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 11:45:20 by tde-sous          #+#    #+#             */
+/*   Updated: 2023/10/25 12:35:11 by tde-sous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+
+class Contact
+{
+	private:
+		std::string FirstName;
+		std::string LastName;
+		std::string NickName;
+		std::string PhoneNumber;
+		std::string DarkestSecret;
+	public:
+		void SetContact(std::string First, std::string Last, std::string Nick, std::string Phone, std::string Secret)
+		{
+			this->FirstName = First;
+			this->LastName = Last;
+			this->NickName = Nick;
+			this->PhoneNumber = Phone;
+			this->DarkestSecret = Secret;
+		}
+		void PrintContact()
+		{
+			std::cout << (this->FirstName + "\n" + this->LastName + "\n" + this->NickName + "\n" + this->PhoneNumber + "\n" + this->DarkestSecret);
+		}
+};
+
+class PhoneBook
+{
+	private:
+		Contact contacts[8];
+	public:
+
+};
+
---
+++ CPP00/ex01/main.cpp
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 11:51:20 by tde-sous          #+#    #+#             */
+/*   Updated: 2023/10/25 12:42:48 by tde-sous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.hpp"
+
+
+int	main()
+{
+	Contact contact;
+
+	std::cout << "Welcome to your phonebook!\n";
+	//sleep(2);
+	std::system("clear");
+	std::cout << "Loading...\n";
+	//sleep(1);
+	std::system("clear");
+	while(1)
+	{
+		std::cout << "Please input...\n";
+		std::string input;
+		std::cin >> input;
+		std::system("clear");
+		if (input == "ADD")
+		{
+			std::string First;
+			std::string Last;
+			std::string Nick;
+			std::string Phone;
+			std::string Secret;
+
+			std::system("clear");
+
+			std::cout << "First Name: ";
+			std::cin >> First;
+			if (First.empty())
+				std::cout << "You can't have empty fields. Try again...\n";
+
+			std::cout << "\nLast Name: ";
+			std::cin >> Last;
+			if (Last.empty())
+				std::cout << "You can't have empty fields. Try again...\n";
+
+			std::cout << "\nNick Name: ";
+			std::cin >> Nick;
+			if (Nick.empty())
+				std::cout << "You can't have empty fields. Try again...\n";
+
+			std::cout << "\nPhone Number: ";
+			std::cin >> Phone;
+			if (Phone.empty())
+				std::cout << "You can't have empty fields. Try again...\n";
+
+			std::cout << "\nDarkest Secret: ";
+			std::cin >> Secret;
+			if (Secret.empty())
+				std::cout << "You can't have empty fields. Try again...\n";
+
+			contact.SetContact(First, Last, Nick, Phone, Secret);
+			std::cout << "\nPrinting info: ";
+			contact.PrintContact();
+		}
+		else if (input == "SEARCH")
+		{
+
+		}
+		else if (input == "EXIT")
+		{
+			return (0);
+		}
+	}
+}
