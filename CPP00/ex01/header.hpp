45f038c4d1e473bca0dba6db349413a221658320
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-11-05 12:53:11 +0000

    Better number verification

--- CPP00/ex01/header.hpp
+++ CPP00/ex01/header.hpp
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 11:45:20 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/10/27 12:23:20 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/11/05 11:47:16 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,70 +14,169 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sstream>
 
-class Contact
-{
-	private:
-		std::string FirstName;
-		std::string LastName;
-		std::string NickName;
-		std::string PhoneNumber;
-		std::string DarkestSecret;
-		bool Initialized;
-	public:
-		Contact() : Initialized(false)
-		{}
-		void SetContact(std::string First, std::string Last, std::string Nick, std::string Phone, std::string Secret)
-		{
-			this->FirstName = First;
-			this->LastName = Last;
-			this->NickName = Nick;
-			this->PhoneNumber = Phone;
-			this->DarkestSecret = Secret;
-			this->Initialized = true;
-		}
-		void PrintContact()
-		{
-			std::cout << (this->FirstName + "\n" + this->LastName + "\n" + this->NickName + "\n" + this->PhoneNumber + "\n" + this->DarkestSecret + "\n");
-		}
-		int	ContactExists()
-		{
-			return (Initialized);
-		}
+# define MaxLength 10 /* Use bigger then 10 */
+# define MaxId 8      /* Max Id's to show */
+
+class Contact {
+private:
+    std::string FirstName;
+    std::string LastName;
+    std::string Nickname;
+    std::string PhoneNumber;
+    std::string DarkestSecret;
+    bool Initialized;
+public:
+    Contact() : Initialized(false) {}
+
+    void SetContact(std::string First, std::string Last, std::string Nick, std::string Phone, std::string Secret) {
+	    this->FirstName = First;
+	    this->LastName = Last;
+	    this->Nickname = Nick;
+	    this->PhoneNumber = Phone;
+	    this->DarkestSecret = Secret;
+	    this->Initialized = true;
+    }
+
+    void PrintContact() {
+	    std::cout << (this->FirstName + "\n" + this->LastName + "\n" + this->Nickname + "\n" + this->PhoneNumber +
+				"\n" + this->DarkestSecret + "\n");
+    }
+
+    int ContactExists() {
+	    return (Initialized);
+    }
+
+    std::string GetFirstName() {
+	    return (this->FirstName);
+    }
+
+    std::string GetLastName() {
+	    return (this->LastName);
+    }
+
+    std::string GetNickname() {
+	    return (this->Nickname);
+    }
+
 };
 
-class PhoneBook
-{
-	private:
-		Contact Contacts[8];
-		void	PrintHeader()
-		{
-			for(int i = 0; i < 45; i++)
-				std::cout << "_";
-			std::cout << "\n";
-		}
-		void	PrintList()
-		{
-			std::cout << "|     Index|First Name| Last Name|  Nickname|";
-		}
-	public:
-		void	SetContactId(int id, std::string First, std::string Last, std::string Nick, std::string Phone, std::string Secret)
-		{
-			Contacts[id].SetContact(First, Last, Nick, Phone, Secret);
-		}
-		void	IdPrintContact(int id)
-		{
-			Contacts[id].PrintContact();
-		}
-		int	isContactUsed(int id)
-		{
-			return (this->Contacts[id].ContactExists());
-		}
-		void	ListPhoneBook()
-		{
-			PrintHeader();
-			PrintList();
-			//PrintBottom();
-		}
+class PhoneBook {
+private:
+    Contact Contacts[MaxId];
+
+    void PrintHeader() {
+	    for (int i = 0; i < (MaxLength * 4 + 5); i++)
+		    std::cout << "_";
+	    std::cout << "\n";
+	    std::cout << "|     Index|First Name| Last Name|  Nickname|\n";
+    }
+
+    void PrintListForm(int id) {
+	    std::cout << "|";
+	    std::stringstream ss;
+	    ss << id;
+	    std::string idtxt = ss.str();
+
+	    for (int i = (MaxLength - idtxt.size()); i > 0; i--)
+		    std::cout << ' ';
+	    std::cout << id;
+	    std::cout << "|";
+	    std::string FirstName = Contacts[id].GetFirstName();
+	    if (FirstName.size() > MaxLength) {
+		    FirstName.resize(MaxLength);
+		    FirstName[MaxLength - 1] = '.';
+	    } else {
+		    for (int i = (MaxLength - FirstName.size()); i > 0; i--)
+			    std::cout << ' ';
+	    }
+	    std::cout << FirstName;
+	    std::cout << "|";
+
+	    std::string LastName = Contacts[id].GetLastName();
+	    if (LastName.size() > MaxLength) {
+		    LastName.resize(MaxLength);
+		    LastName[MaxLength - 1] = '.';
+	    } else {
+		    for (int i = (MaxLength - LastName.size()); i > 0; i--)
+			    std::cout << ' ';
+	    }
+	    std::cout << LastName;
+	    std::cout << "|";
+
+	    std::string Nickname = Contacts[id].GetNickname();
+	    if (Nickname.size() > MaxLength) {
+		    Nickname.resize(MaxLength);
+		    Nickname[MaxLength - 1] = '.';
+	    } else {
+		    for (int i = (MaxLength - Nickname.size()); i > 0; i--)
+			    std::cout << ' ';
+	    }
+	    std::cout << Nickname;
+	    std::cout << "|\n";
+    }
+
+    void PrintBottom() {
+	    for (int i = 0; i < 45; i++)
+		    std::cout << "";
+    }
+
+    void PrintMiddle() {
+	    for (
+			int id = 0;
+			id <= MaxId; id++) {
+		    if (Contacts[id].GetFirstName().empty())
+			    break;
+		    PrintListForm(id);
+	    }
+    }
+
+    std::string GetFirstName(int id) {
+	    return (Contacts[id].GetFirstName());
+    }
+
+    std::string GetLastName(int id) {
+	    return (Contacts[id].GetLastName());
+    }
+
+    std::string GetNickname(int id) {
+	    return (Contacts[id].GetNickname());
+    }
+
+public:
+    void
+    SetContactId(int id, std::string First, std::string Last, std::string Nick, std::string Phone, std::string Secret) {
+	    Contacts[id].SetContact(First, Last, Nick, Phone, Secret);
+    }
+
+    void IdPrintContact(int id) {
+	    Contacts[id].PrintContact();
+    }
+
+    int isContactUsed(int id) {
+	    return (this->Contacts[id].ContactExists());
+    }
+
+    void ListPhoneBook() {
+	    if (!Contacts[0].ContactExists()) {
+		    std::cout << "No contacts on the list";
+		    return;
+	    }
+	    PrintHeader();
+	    PrintMiddle();
+	    std::cout << "\nType id for full informations: ";
+	    std::string input;
+	    std::cin >> input;
+	    const char *input2 = input.c_str();
+	    char *input3;
+	    int id = atoi(input2);
+	    strtol(input2, &input3, 10);
+	    if (*input3 == 0 || id < 0 || id > 10 || !Contacts[id].ContactExists()) {
+		    std::cout << "Invalid ID";
+		    return;
+	    }
+	    std::cout << "ID is:" << id;
+    }
 };
 
--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 11:51:20 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/10/27 12:21:32 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/10/27 13:15:26 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ int	main()
 			while (Book.isContactUsed(id))
 			{
 				id++;
-				if (id == 8)
+				if (id == MaxId)
 				{
 					id = 0;
 				}
