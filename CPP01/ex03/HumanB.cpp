673e58d1bde1f120ebdae22cb96f0b3a16586bcf
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-11-19 18:23:51 +0000

    Add: HumanB functions

--- CPP01/ex03/HumanB.cpp
+++ CPP01/ex03/HumanB.cpp
@@ -2,3 +2,14 @@
 //
 
 #include "HumanB.hpp"
+
+HumanB::HumanB(std::string TypeName) : _weapon(0), _name(TypeName)  {
+}
+
+void HumanB::setWeapon(Weapon &TypeClub) {
+	_weapon = &TypeClub;
+}
+
+void HumanB::attack() {
+	std::cout << _name << " attacks with their " << _weapon->getType() << std::endl;
+}
--- CPP01/ex03/HumanB.hpp
+++ CPP01/ex03/HumanB.hpp
@@ -4,9 +4,16 @@
 #ifndef EX03_HUMANB_HPP
 #define EX03_HUMANB_HPP
 
+#include "Weapon.hpp"
 
 class HumanB {
-
+private:
+    Weapon *_weapon;
+    std::string _name;
+public:
+    explicit HumanB(std::string TypeName);
+    void setWeapon(Weapon &TypeClub);
+    void attack();
 };
 
 
