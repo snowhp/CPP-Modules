9ec60df5160b237739713f52b7c7ff61f81beebd
snowhp <13867481+snowhp@users.noreply.github.com>
2023-11-24 14:36:02 +0000

    Add: replace function

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,14 +6,29 @@
 #include <fstream>
 #include <string>
 
+std::string replaceAll(const std::string& str, const std::string& from, const std::string& to) {
+    std::string result;
+    size_t startPos = 0;
+    size_t foundPos;
+
+    while ((foundPos = str.find(from, startPos)) != std::string::npos) {
+        result.append(str, startPos, foundPos - startPos);
+        result.append(to);
+        startPos = foundPos + from.length();
+    }
+
+    result.append(str, startPos, str.length() - startPos);
+    return result;
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 4)
-        return(std::cout << "Not enough arguments" << std::endl, 0);
+        return(std::cout << "Not enough arguments" << std::endl, 1);
     std::fstream file;
     file.open(argv[1], std::ios::in);
     if (file.fail())
-        return (std::cout << "File can't be opened!" << std::endl, 0);
+        return (std::cout << "File can't be opened!" << std::endl, 1);
     else {
         std::cout << "File opened!" << std::endl;
         std::fstream file_replace;
@@ -24,10 +39,7 @@ int main(int argc, char **argv)
         if (file.is_open())
         {
             while(getline(file, line))
-            {
-                std::cout << line << std::endl;
-                file_replace << line << std::endl;
-            }
+                file_replace << replaceAll(line, argv[2], argv[3]) << std::endl;
         }
         file_replace.close();
         file.close();
