32edb9c6f7b59e7cae5c2b3bc71f1459329a48bf
snowhp <13867481+snowhp@users.noreply.github.com>
2023-11-28 17:08:16 +0000

    Add: Copy from ex05 to ex06

---
+++ CPP01/ex06/Harl.hpp
@@ -0,0 +1,23 @@
+//
+// Created by tde-sous on 11/27/23.
+//
+
+#ifndef CPP01_HARL_HPP
+#define CPP01_HARL_HPP
+
+#include <string>
+#include <iostream>
+#include <cstdlib>
+
+class Harl {
+private:
+    void debug( void );
+    void info( void );
+    void warning( void );
+    void error( void );
+public:
+    void complain( std::string level );
+};
+
+
+#endif //CPP01_HARL_HPP
---
+++ CPP01/ex06/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by tde-sous on 11/27/23.
+//
+
+#include "Harl.hpp"
+
+int main()
+{
+    Harl testclass;
+    testclass.complain("0");
+    testclass.complain("1");
+    testclass.complain("2");
+    testclass.complain("3");
+}
