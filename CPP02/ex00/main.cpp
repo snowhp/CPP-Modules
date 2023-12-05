3075f6d9dd489990199f3cadce4b4de3ccf589f6
SnowHP <13867481+snowhp@users.noreply.github.com>
2023-12-05 12:14:40 +0000

    Add: Main file

--- CPP02/ex00/main.cpp
+++ CPP02/ex00/main.cpp
@@ -1,1 +1,13 @@
 // Created by tde-sous on 11/29/23.
+#include <iostream>
+
+int main(void) {
+    Fixed a;
+    Fixed b(a);
+    Fixed c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return 0;
+}
