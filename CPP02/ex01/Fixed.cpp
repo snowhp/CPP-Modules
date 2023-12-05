50c6e7d8ed0c531e2ac65e79433769c1228c5db9
SnowHP <13867481+snowhp@users.noreply.github.com>
2023-12-05 12:25:22 +0000

    Add: Base files

---
+++ CPP02/ex01/Fixed.cpp
@@ -0,0 +1,34 @@
+// Created by tde-sous on 11/29/23.
+
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed() : fpn(0) {
+    std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const Fixed &other) : fpn(other.fpn) {
+    // Copy constructor implementation
+    std::cout << "Copy constructor called\n";
+}
+
+Fixed &Fixed::operator=(const Fixed &other) {
+    // Copy assignment operator implementation
+    fpn = other.fpn;
+    std::cout << "Copy assignment operator called\n";
+    return *this;
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called\n";
+    // Destructor implementation
+}
+
+int Fixed::getRawBits(void) const {
+    std::cout << "getRawBits member function called\n";
+    return (fpn);
+}
+
+void Fixed::setRawBits(const int raw) {
+    fpn = raw;
+}
---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,22 @@
+// Created by tde-sous on 11/29/23.
+
+#ifndef EX00_FIXED_HPP
+#define EX00_FIXED_HPP
+
+
+class Fixed {
+private:
+    int fpn;
+    static const int fractional = 8;
+public:
+    Fixed(); // Default Constructor
+    Fixed(const Fixed &other); // Copy Constructor
+    Fixed &operator=(const Fixed &other); // Copy Assignment Operator
+    ~Fixed(); // Destructor
+    int getRawBits(void) const;
+
+    void setRawBits(int const raw);
+};
+
+
+#endif //EX00_FIXED_HPP
