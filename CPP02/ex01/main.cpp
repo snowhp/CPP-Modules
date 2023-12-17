d4cae0f800beb1008fb038debbd29cc5372e034e
snowhp <[13867481+snowhp@users.noreply.github.com](mailto:13867481+snowhp@users.noreply.github.com)>
2023-12-17 19:10:56 +0000

    Style: Format main tests from subject to consistent style

--- CPP02/ex01/main.cpp
+++ CPP02/ex01/main.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 #include "Fixed.hpp"
 
-int main( void ) {
+int main(void) {
     Fixed a;
-    Fixed const b( 10 );
-    Fixed const c( 42.42f );
-    Fixed const d( b );
-    a = Fixed( 1234.4321f );
+    Fixed const b(10);
+    Fixed const c(42.42f);
+    Fixed const d(b);
+    a = Fixed(1234.4321f);
     std::cout << "a is " << a << std::endl;
     std::cout << "b is " << b << std::endl;
     std::cout << "c is " << c << std::endl;
