1e901cdb16753adf14154055257b521e4c653aa9
SnowHP <13867481+snowhp@users.noreply.github.com>
2023-12-19 13:30:06 +0000

    Add: Operator << overload

--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -3,23 +3,26 @@
 #ifndef EX00_FIXED_HPP
 #define EX00_FIXED_HPP
 
+#include <iostream>
 
 class Fixed {
 private:
-    int fpn;
-    static const int fractional = 8;
+  int fpn;
+  static const int fractional = 8;
+
 public:
-    Fixed(int nb_integer);
-    Fixed(float nb_float);
-    Fixed(); // Default Constructor
-    Fixed(const Fixed &other); // Copy Constructor
-    Fixed &operator=(const Fixed &other); // Copy Assignment Operator
-    ~Fixed(); // Destructor
-    int getRawBits(void) const;
-    void setRawBits(int const raw);
-    float toFloat( void ) const;
-    int toInt( void ) const;
+  Fixed(int nb_integer);
+  Fixed(float nb_float);
+  Fixed();                              // Default Constructor
+  Fixed(const Fixed &other);            // Copy Constructor
+  Fixed &operator=(const Fixed &other); // Copy Assignment Operator
+  ~Fixed();                             // Destructor
+  int getRawBits(void) const;
+  void setRawBits(int const raw);
+  float toFloat(void) const;
+  int toInt(void) const;
 };
 
+std::ostream &operator<<(std::ostream &out, const Fixed &right);
 
-#endif //EX00_FIXED_HPP
+#endif // EX01_FIXED_HPP
