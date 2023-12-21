d6fc51d981d1e161af3e3a5e259d2371f9f09a12
SnowHP <13867481+snowhp@users.noreply.github.com>
2023-12-21 17:26:51 +0000

    Add: Tests for operators added ">, <, >=, <=, == and !="

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -2,18 +2,21 @@
 #include "Fixed.hpp"
 
 int main(void) {
-    Fixed a;
-    Fixed const b(10);
-    Fixed const c(42.42f);
-    Fixed const d(b);
-    a = Fixed(1234.4321f);
-    std::cout << "a is " << a << std::endl;
-    std::cout << "b is " << b << std::endl;
-    std::cout << "c is " << c << std::endl;
-    std::cout << "d is " << d << std::endl;
-    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-    return 0;
+  Fixed a(3);
+  Fixed const b(2);
+  std::cout << "A(" << a.toFloat() << ") is greater than B(" << b.toFloat() << "): " << (a > b) << std::endl;
+  std::cout << "A(" << a.toFloat() << ") is less than B(" << b.toFloat() << "): " << (a < b) << std::endl;
+  std::cout << "A(" << a.toFloat() << ") is greater or equal than B(" << b.toFloat() << "): " << (a >= b) << std::endl;
+  std::cout << "A(" << a.toFloat() << ") is less than or equal to B(" << b.toFloat() << "): " << (a <= b) << std::endl;
+  std::cout << "A(" << a.toFloat() << ") is equal to B(" << b.toFloat() << "): " << (a == b) << std::endl;
+  std::cout << "A(" << a.toFloat() << ") is different than B(" << b.toFloat() << "): " << (a != b) << std::endl;
+
+  /*std::cout << a << std::endl;
+  std::cout << ++a << std::endl;
+  std::cout << a << std::endl;
+  std::cout << a++ << std::endl;
+  std::cout << a << std::endl;
+  std::cout << b << std::endl;
+  std::cout << Fixed::max(a, b) << std::endl;*/
+  return 0;
 }
