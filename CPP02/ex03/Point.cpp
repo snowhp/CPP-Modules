512d89cfc501f9eb93a6660d05b9d0d36dd60431
snowhp <13867481+snowhp@users.noreply.github.com>
2024-01-13 16:59:51 +0000

    Add: Point class files

--- CPP02/ex03/Point.cpp
+++ CPP02/ex03/Point.cpp
@@ -1,3 +1,20 @@
-// Created by user on 13-01-2024.
-
+// Created by tde-sous on 13-01-2024.
 #include "Point.hpp"
+
+
+Point::Point() {
+    // Default constructor implementation
+}
+
+Point::Point(const Point &other) {
+    // Copy constructor implementation
+}
+
+Point &Point::operator=(const Point &other) {
+    // Copy assignment operator implementation
+    return *this;
+}
+
+Point::~Point() {
+    // Destructor implementation
+}
--- CPP02/ex03/Point.hpp
+++ CPP02/ex03/Point.hpp
@@ -1,11 +1,14 @@
-// Created by user on 13-01-2024.
-
+// Created by tde-sous on 13-01-2024.
 #ifndef EX03_POINT_HPP
 #define EX03_POINT_HPP
 
 
 class Point {
-
+public:
+    Point(); // Default Constructor
+    Point(const Point &other); // Copy Constructor
+    Point &operator=(const Point &other); // Copy Assignment Operator
+    ~Point(); // Destructor
 };
 
 
