7ed7ece389f889194129b793f79939af7c4a3078
snowhp <13867481+snowhp@users.noreply.github.com>
2024-01-13 16:51:29 +0000

    Add: Point class files

---
+++ CPP02/ex03/Point.cpp
@@ -0,0 +1,3 @@
+// Created by user on 13-01-2024.
+
+#include "Point.hpp"
---
+++ CPP02/ex03/Point.hpp
@@ -0,0 +1,12 @@
+// Created by user on 13-01-2024.
+
+#ifndef EX03_POINT_HPP
+#define EX03_POINT_HPP
+
+
+class Point {
+
+};
+
+
+#endif //EX03_POINT_HPP
