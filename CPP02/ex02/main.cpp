5983efa9b69bfa648aba3cc45d9a7f0ff96e1231
snowhp <13867481+snowhp@users.noreply.github.com>
2024-01-13 16:28:31 +0000

    Add: Tests from subject

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -1,7 +1,7 @@
 // Created by tde-sous on 12/19/23.
 #include "Fixed.hpp"
 
-int main(void) {
+/*int main(void) {
     Fixed a(3);
     Fixed const b(2);
     std::cout << "A(" << a.toFloat() << ") is greater than B(" << b.toFloat() << "): " << (a > b) << std::endl;
@@ -44,5 +44,18 @@ int main(void) {
     std::cout << "The min between A and B is " << Fixed::min(a, b) << std::endl;
 
     std::cout << "The max between A and B is " << Fixed::max(a, b) << std::endl;
+    return 0;
+}*/
+
+int main( void ) {
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
     return 0;
 }
