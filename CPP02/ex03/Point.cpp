737e2c5badbe4b7206e79173320f720898e15867
snowhp <13867481+snowhp@users.noreply.github.com>
2024-01-14 18:33:49 +0000

    Add: Class Point Canonical added

--- CPP02/ex03/Point.cpp
+++ CPP02/ex03/Point.cpp
@@ -3,21 +3,34 @@
 
 
 Point::Point() : x(Fixed(0)), y(Fixed(0)){
-    // Default constructor implementation
+    std::cout << "Default constructor called\n";
 }
 
 Point::Point(Fixed x, Fixed y) : x(x), y(y) {
+    std::cout << "Constructor called\n";
 }
 
-Point::Point(const Point &other) {
-    // Copy constructor implementation
+Point::Point(const Point &other) : x(other.x), y(other.y) {
+    std::cout << "Copy constructor called\n";
 }
 
-Point &Point::operator=(const Point &other) {
-    // Copy assignment operator implementation
+Point &Point::operator=(Point other) {
+    if (this == &other)
+        return *this;
+    (Fixed)this->y = other.getY();
+    (Fixed)this->x = other.getX();
+    std::cout << "Copy assignment operator called\n";
     return *this;
 }
 
 Point::~Point() {
     // Destructor implementation
+}
+
+Fixed Point::getX() {
+    return (this->x);
+}
+
+Fixed Point::getY() {
+    return (this->y);
 }
