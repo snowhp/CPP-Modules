4a4c5d07c995039e1e348f6374008029a9becae9
snowhp <13867481+snowhp@users.noreply.github.com>
2024-01-14 23:10:09 +0000

    Add: Canonical form + getter's

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -1,19 +1,38 @@
 // Created by tde-sous on 14-01-2024.
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap() {
-  // Default constructor implementation
+ClapTrap::ClapTrap(std::string Name) {
+  std::cout << "Default Constructor called\n";
+  this->_Name = Name;
+  this->_HitPoints = 10;
+  this->_EnergyPoints = 10;
+  this->_AttackDamage = 0;
 }
 
 ClapTrap::ClapTrap(const ClapTrap &other) {
-  // Copy constructor implementation
+  std::cout << "Copy constructor called\n";
+  *this = other;
 }
 
 ClapTrap &ClapTrap::operator=(const ClapTrap &other) {
-  // Copy assignment operator implementation
+  std::cout << "Copy assignment operator called\n";
+  if (this == &other)
+    return *this;
+  this->_Name = other.getName();
+  this->_HitPoints = other.getHitPoints();
+  this->_EnergyPoints = other.getEnergyPoints();
+  this->_AttackDamage = other.getAttackDamage();
   return *this;
 }
 
 ClapTrap::~ClapTrap() {
+  std::cout << "Destructor called\n";
   // Destructor implementation
 }
+
+std::string ClapTrap::getName() const { return (this->_Name); }
+
+int ClapTrap::getHitPoints() const { return (this->_HitPoints); }
+
+int ClapTrap::getEnergyPoints() const { return (this->_EnergyPoints); }
+int ClapTrap::getAttackDamage() const { return (this->_AttackDamage); }
--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -2,12 +2,27 @@
 #ifndef EX00_CLAPTRAP_HPP
 #define EX00_CLAPTRAP_HPP
 
+#include <iostream>
+#include <string>
+
 class ClapTrap {
+private:
+  std::string _Name; // Name of the ClapTrap
+  int _HitPoints;    // Represent the health of the ClapTrap
+  int _EnergyPoints; // Energy Points
+  int _AttackDamage; // Attack Damage
 public:
-  ClapTrap();                                 // Default Constructor
+  ClapTrap(std::string Name);                 // Default Constructor
   ClapTrap(const ClapTrap &other);            // Copy Constructor
   ClapTrap &operator=(const ClapTrap &other); // Copy Assignment Operator
   ~ClapTrap();                                // Destructor
+  std::string getName() const;
+  int getHitPoints() const;
+  int getEnergyPoints() const;
+  int getAttackDamage() const;
+  void attack(const std::string &target);
+  void takeDamage(unsigned int amount);
+  void beRepaired(unsigned int amount);
 };
 
 #endif // EX00_CLAPTRAP_HPP
