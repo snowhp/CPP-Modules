70a1a9a87b83fa4e2058c3848d0145f6c90d2165
snowhp <13867481+snowhp@users.noreply.github.com>
2024-01-18 18:41:35 +0000

    Refactor: Comments in .hpp file and better variable names
    Added comments to the class ClapTrap and changed the names of the private variables to have a underscore after their name

--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -1,30 +1,105 @@
 // Created by tde-sous on 14-01-2024.
+
 #ifndef EX00_CLAPTRAP_HPP
 #define EX00_CLAPTRAP_HPP
 
 #include <iostream>
 #include <string>
 
+/**
+ * @class ClapTrap
+ * @brief This class represents a ClapTrap.
+ */
 class ClapTrap {
 private:
-  std::string _Name; // Name of the ClapTrap
-  int _HitPoints;    // Represent the health of the ClapTrap
-  int _EnergyPoints; // Energy Points
-  int _AttackDamage; // Attack Damage
+  std::string Name_; /**< Name of the ClapTrap */
+  int HitPoints_;    /**< Represent the health of the ClapTrap */
+  int EnergyPoints_; /**< Energy Points */
+  int AttackDamage_; /**< Attack Damage */
+
 public:
-  explicit ClapTrap(std::string Name);                 // Default Constructor
-  ClapTrap(const ClapTrap &other);            // Copy Constructor
-  ClapTrap &operator=(const ClapTrap &other); // Copy Assignment Operator
-  ~ClapTrap();                                // Destructor
+  /**
+   * @brief Default constructor.
+   * @param Name The name of the ClapTrap.
+   */
+  explicit ClapTrap(std::string Name);
+
+  /**
+   * @brief Copy constructor.
+   * @param other The other ClapTrap to copy.
+   */
+  ClapTrap(const ClapTrap &other);
+
+  /**
+   * @brief Copy assignment operator.
+   * @param other The other ClapTrap to assign.
+   * @return A reference to the assigned ClapTrap.
+   */
+  ClapTrap &operator=(const ClapTrap &other);
+
+  /**
+   * @brief Destructor.
+   */
+  ~ClapTrap();
+
+    /**
+   * @brief Get the name of the ClapTrap.
+   * @return The name of the ClapTrap.
+   */
   std::string getName() const;
+
+  /**
+   * @brief Get the hit points of the ClapTrap.
+   * @return The hit points of the ClapTrap.
+   */
   int getHitPoints() const;
+
+  /**
+   * @brief Get the energy points of the ClapTrap.
+   * @return The energy points of the ClapTrap.
+   */
   int getEnergyPoints() const;
+
+  /**
+   * @brief Get the attack damage of the ClapTrap.
+   * @return The attack damage of the ClapTrap.
+   */
   int getAttackDamage() const;
+
+  /**
+   * @brief Makes the ClapTrap attack a target.
+   * @param target The name of the target to attack.
+   */
   void attack(const std::string &target);
+
+  /**
+   * @brief Reduces the health points of the ClapTrap.
+   * @param amount The amount of damage to take.
+   */
   void takeDamage(unsigned int amount);
+
+  /**
+   * @brief Increases the health points of the ClapTrap.
+   * @param amount The amount of health to repair.
+   */
   void beRepaired(unsigned int amount);
-  bool isFunctional() const; // True if have energy point and hit points else false
+
+  /**
+   * @brief Checks if the ClapTrap is functional.
+   * @return True if the ClapTrap has energy points and hit points, false otherwise.
+   */
+  bool isFunctional() const;
+
+  /**
+   * @brief Increases the health points of the ClapTrap.
+   * @param amount The amount of health to add.
+   */
   void addHitPoints(int amount);
+
+  /**
+   * @brief Increases the energy points of the ClapTrap.
+   * @param amount The amount of energy to add.
+   */
   void addEnergyPoints(int amount);
 };
 
