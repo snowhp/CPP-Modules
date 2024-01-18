e07f0a37d31c153aeb0cb0d050876e2058e33a16
snowhp <13867481+snowhp@users.noreply.github.com>
2024-01-18 21:41:25 +0000

    Test: First tests added to the main

--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -1,6 +1,6 @@
 // Created by tde-sous on 14-01-2024.
 
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main()
 {
@@ -9,5 +9,22 @@ int main()
   MachineA.attack(MachineB.getName());
   MachineB.takeDamage(5);
   MachineA.beRepaired(1);
+
+  std::cout << std::endl << std::endl;
+
+  ScavTrap ScavA("Robert");
+  ScavA.guardGate();
+  ScavA.attack("Pigeon");
+  ScavA.takeDamage(10);
+
+  ScavTrap ScavB(ScavA);
+  ScavB.beRepaired(5);
+  ScavB.beRepaired(1);
+
+  ScavTrap ScavC = ScavA;
+  ScavC.takeDamage(100);
+  ScavC.takeDamage(100);
+  std::cout << ScavC;
+  ScavC.attack("Perry");
   return 0;
 }
