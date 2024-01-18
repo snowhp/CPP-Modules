cf27a50c306163fc9aa37ae177bfb300d6811d7c
snowhp <13867481+snowhp@users.noreply.github.com>
2024-01-18 21:16:00 +0000

    Add: Canocial form for ScavTrap function

---
+++ CPP03/ex01/ScavTrap.cpp
@@ -0,0 +1,26 @@
+// Created by tde-sous on 18-01-2024.
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap(const std::string &Name) : ClapTrap(Name) {
+  std::cout << "[ScavTrap] Default constructor called\n";
+  HitPoints_ = 100;
+  EnergyPoints_ = 50;
+  AttackDamage_ = 20;
+}
+
+ScavTrap::ScavTrap(const ScavTrap &other) : ClapTrap(other) {
+  std::cout << "[ScavTrap] Copy constructor called\n";
+}
+
+ScavTrap &ScavTrap::operator=(const ScavTrap &other) {
+  std::cout << "[ScavTrap] Copy assignment operator called\n";
+  if (this == &other)
+    return *this;
+  Name_ = other.getName();
+  HitPoints_ = other.getHitPoints();
+  EnergyPoints_ = other.getEnergyPoints();
+  AttackDamage_ = other.getAttackDamage();
+  return *this;
+}
+
+ScavTrap::~ScavTrap() { std::cout << "[ScavTrap] Destructor called\n"; }
