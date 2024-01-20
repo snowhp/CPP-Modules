cdb00f4f2f8e2dc457382cd70a2090f17b0e74fc
snowhp <13867481+snowhp@users.noreply.github.com>
2024-01-20 15:21:51 +0000

    Fix: Canonical Form implementation of the functions

---
+++ CPP03/ex02/FragTrap.hpp
@@ -0,0 +1,30 @@
+// Created by tde-sous on 20-01-2024.
+#ifndef EX02_FRAGTRAP_HPP
+#define EX02_FRAGTRAP_HPP
+
+#include "ScavTrap.hpp"
+
+class FragTrap : public ScavTrap {
+public:
+  /**
+   * @brief Default constructor.
+   */
+  FragTrap(const std::string &Name);
+  /**
+   * @brief Copy constructor.
+   * @param other The other FragTrap to copy.
+   */
+  FragTrap(const FragTrap &other);
+  /**
+   * @brief Copy assignment operator.
+   * @param other The other FragTrap to assign.
+   * @return A reference to the assigned FragTrap.
+   */
+  FragTrap &operator=(const FragTrap &other);
+  /**
+   * @brief Destructor.
+   */
+  ~FragTrap();
+};
+
+#endif // EX02_FRAGTRAP_HPP
