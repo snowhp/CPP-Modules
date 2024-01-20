1fd3fe4b3bffcf6264e011c1c1526be16eeffe72
snowhp <13867481+snowhp@users.noreply.github.com>
2024-01-20 15:41:56 +0000

    Add: More tests to main function

--- CPP03/ex02/main.cpp
+++ CPP03/ex02/main.cpp
@@ -1,11 +1,14 @@
 // Created by tde-sous on 14-01-2024.
 
-#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
-int main()
-{
+int main() {
   ClapTrap MachineA("Roboto");
   ClapTrap MachineB("Roboto");
+  std::cout << "I am " << MachineA.getName() << std::endl
+            << "HitPoints: " << MachineA.getHitPoints() << std::endl
+            << "EnergyPoints: " << MachineA.getEnergyPoints() << std::endl
+            << "AttackDamage: " << MachineA.getAttackDamage() << std::endl;
   MachineA.attack(MachineB.getName());
   MachineB.takeDamage(5);
   MachineA.beRepaired(1);
@@ -13,6 +16,10 @@ int main()
   std::cout << std::endl << std::endl;
 
   ScavTrap ScavA("Robert");
+  std::cout << "I am " << ScavA.getName() << std::endl
+            << "HitPoints: " << ScavA.getHitPoints() << std::endl
+            << "EnergyPoints: " << ScavA.getEnergyPoints() << std::endl
+            << "AttackDamage: " << ScavA.getAttackDamage() << std::endl;
   ScavA.guardGate();
   ScavA.attack("Pigeon");
   ScavA.takeDamage(10);
@@ -25,5 +32,25 @@ int main()
   ScavC.takeDamage(100);
   ScavC.takeDamage(100);
   ScavC.attack("Perry");
+
+  FragTrap FragA("Michele");
+  std::cout << "I am " << FragA.getName() << std::endl
+            << "HitPoints: " << FragA.getHitPoints() << std::endl
+            << "EnergyPoints: " << FragA.getEnergyPoints() << std::endl
+            << "AttackDamage: " << FragA.getAttackDamage() << std::endl;
+  for (int i = 0; i <= 100; i++) {
+    std::cout << FragA.getEnergyPoints() << " ";
+    FragA.attack("Tester");
+  }
+
+  FragTrap FragB(FragA);
+  std::cout << FragB.getEnergyPoints() << " ";
+  FragB.attack("Tester");
+
+  FragTrap FragC("Replaced");
+  FragC = FragB;
+  std::cout << FragC.getEnergyPoints() << " ";
+  FragC.attack("Tester");
+
   return 0;
 }
