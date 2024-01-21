146a6ac80f8e819758c91a7023983cbc5517211e
snowhp <13867481+snowhp@users.noreply.github.com>
2024-01-21 18:32:24 +0000

    Tests: Add WrongAnimal and WrongCat and more tests to main

--- CPP04/ex00/main.cpp
+++ CPP04/ex00/main.cpp
@@ -2,15 +2,26 @@
 
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include "WrongCat.hpp"
 
 int main() {
-  const Animal *meta = new Animal();
-  const Animal *j = new Dog();
-  const Animal *i = new Cat();
-  std::cout << j->getType() << " " << std::endl;
-  std::cout << i->getType() << " " << std::endl;
-  i->makeSound(); // will output the cat sound!
-  j->makeSound();
-  meta->makeSound();
-  return 0;
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); // Outputs the cat sound!
+    j->makeSound();
+    meta->makeSound();
+
+    // Additional tests with WrongCat
+    const WrongAnimal* wrongCat = new WrongCat();
+    wrongCat->makeSound();
+
+    delete meta;
+    delete j;
+    delete i;
+    delete wrongCat;
+
+    return 0;
 }
