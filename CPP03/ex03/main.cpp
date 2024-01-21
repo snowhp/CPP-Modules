b1533ab77ff480af08d6972d8185bf28a4781db5
snowhp <13867481+snowhp@users.noreply.github.com>
2024-01-21 15:04:15 +0000

    Test: Add more tests to the main function for testing DiamondTrap Class

--- CPP03/ex03/main.cpp
+++ CPP03/ex03/main.cpp
@@ -1,56 +1,15 @@
 // Created by tde-sous on 14-01-2024.
 
-#include "FragTrap.hpp"
+#include "DiamondTrap.hpp"
 
 int main() {
-  ClapTrap MachineA("Roboto");
-  ClapTrap MachineB("Roboto");
-  std::cout << "I am " << MachineA.getName() << std::endl
-            << "HitPoints: " << MachineA.getHitPoints() << std::endl
-            << "EnergyPoints: " << MachineA.getEnergyPoints() << std::endl
-            << "AttackDamage: " << MachineA.getAttackDamage() << std::endl;
-  MachineA.attack(MachineB.getName());
-  MachineB.takeDamage(5);
-  MachineA.beRepaired(1);
+  DiamondTrap ClassA("Name");
 
-  std::cout << std::endl << std::endl;
-
-  ScavTrap ScavA("Robert");
-  std::cout << "I am " << ScavA.getName() << std::endl
-            << "HitPoints: " << ScavA.getHitPoints() << std::endl
-            << "EnergyPoints: " << ScavA.getEnergyPoints() << std::endl
-            << "AttackDamage: " << ScavA.getAttackDamage() << std::endl;
-  ScavA.guardGate();
-  ScavA.attack("Pigeon");
-  ScavA.takeDamage(10);
-
-  ScavTrap ScavB(ScavA);
-  ScavB.beRepaired(5);
-  ScavB.beRepaired(1);
-
-  ScavTrap ScavC = ScavA;
-  ScavC.takeDamage(100);
-  ScavC.takeDamage(100);
-  ScavC.attack("Perry");
-
-  FragTrap FragA("Michele");
-  std::cout << "I am " << FragA.getName() << std::endl
-            << "HitPoints: " << FragA.getHitPoints() << std::endl
-            << "EnergyPoints: " << FragA.getEnergyPoints() << std::endl
-            << "AttackDamage: " << FragA.getAttackDamage() << std::endl;
-  for (int i = 0; i <= 100; i++) {
-    std::cout << FragA.getEnergyPoints() << " ";
-    FragA.attack("Tester");
-  }
-
-  FragTrap FragB(FragA);
-  std::cout << FragB.getEnergyPoints() << " ";
-  FragB.attack("Tester");
-
-  FragTrap FragC("Replaced");
-  FragC = FragB;
-  std::cout << FragC.getEnergyPoints() << " ";
-  FragC.attack("Tester");
-
+  std::cout << "I am " << ClassA.getName() << std::endl
+            << "HitPoints: " << ClassA.getHitPoints() << std::endl
+            << "EnergyPoints: " << ClassA.getEnergyPoints() << std::endl
+            << "AttackDamage: " << ClassA.getAttackDamage() << std::endl;
+  ClassA.attack("Pigeon");
+  ClassA.whoAmI();
   return 0;
 }
