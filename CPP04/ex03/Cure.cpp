4fe3eea6375f2ab18c638405df5cb928b9d9fdec
snowhp <13867481+snowhp@users.noreply.github.com>
2024-02-01 17:26:07 +0000

    Fix: Copy constructor and Copy assignment operator working

--- CPP04/ex03/Cure.cpp
+++ CPP04/ex03/Cure.cpp
@@ -5,12 +5,15 @@ Cure::Cure(std::string const &type) : AMateria(type) {
   std::cout << "[Cure] Default constructor called\n";
 }
 
-Cure::Cure(const Cure &other) {
+Cure::Cure(const Cure &other) : AMateria(other) {
   std::cout << "[Cure] Copy constructor called\n";
 }
 
 Cure &Cure::operator=(const Cure &other) {
   std::cout << "[Cure] Copy assignment operator called\n";
+  if (this == &other)
+    return (*this);
+  this->type = other.type;
   return *this;
 }
 
