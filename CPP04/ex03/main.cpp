119955639a24d4785d1c58d7cccbfc9bc10313a2
snowhp <13867481+snowhp@users.noreply.github.com>
2024-02-01 16:36:35 +0000

    Tests: Subjects tests for main comment and standard main constructed

--- CPP04/ex03/main.cpp
+++ CPP04/ex03/main.cpp
@@ -1,1 +1,29 @@
 // Created by tde-sous on 26-01-2024.
+
+// Own tests
+int main()
+{
+
+
+}
+
+// Subject Main test
+/*
+int main() {
+  IMateriaSource *src = new MateriaSource();
+  src->learnMateria(new Ice());
+  src->learnMateria(new Cure());
+  ICharacter *me = new Character("me");
+  AMateria *tmp;
+  tmp = src->createMateria("ice");
+  me->equip(tmp);
+  tmp = src->createMateria("cure");
+  me->equip(tmp);
+  ICharacter *bob = new Character("bob");
+  me->use(0, *bob);
+  me->use(1, *bob);
+  delete bob;
+  delete me;
+  delete src;
+  return 0;
+}*/
