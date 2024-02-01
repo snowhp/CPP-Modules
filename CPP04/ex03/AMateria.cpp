c37905ac6b6adeec23e624be1545f9e48a61d3cb
snowhp <13867481+snowhp@users.noreply.github.com>
2024-02-01 16:36:55 +0000

    Fix: Default message on canonical form messages

--- CPP04/ex03/AMateria.cpp
+++ CPP04/ex03/AMateria.cpp
@@ -1,21 +1,22 @@
 // Created by tde-sous on 26-01-2024.
 #include "AMateria.hpp"
 
-
-
-AMateria::AMateria() {
-    
+AMateria::AMateria(const int &type) {
+  std::cout << "[AMateria] Default constructor called\n";
+}
+AMateria::AMateria(const AMateria &other) {
+  std::cout << "[AMateria] Copy constructor called\n";
 }
 
-AMateria::AMateria(const AMateria& other) {
-    
-}
-
-AMateria& AMateria::operator=(const AMateria& other) {
-    
-    return *this;
+AMateria &AMateria::operator=(const AMateria &other) {
+  std::cout << "[AMateria] Copy assignment operator called\n";
+  return *this;
 }
 
 AMateria::~AMateria() {
-    
+  std::cout << "[AMateria] Destructor called\n";
 }
+
+const int &AMateria::getType() const {}
+
+void AMateria::use(int &target) {}
