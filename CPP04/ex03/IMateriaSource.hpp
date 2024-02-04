d0a92633a45c7d90e50bd0a86cc7a603a19e8f2b
snowhp <13867481+snowhp@users.noreply.github.com>
2024-02-04 22:53:51 +0000

    Add: Interface IMateriaSource.hpp added

---
+++ CPP04/ex03/IMateriaSource.hpp
@@ -0,0 +1,19 @@
+// Created by tde-sous on 03-02-2024.
+#ifndef IMATERIASOURCE_HPP
+#define IMATERIASOURCE_HPP
+
+#include "AMateria.hpp"
+
+class AMateria;
+
+class IMateriaSource
+{
+ public:
+ virtual ~IMateriaSource() {}
+ virtual void learnMateria(AMateria*) = 0;
+ virtual AMateria* createMateria(std::string const & type) = 0;
+};
+
+
+
+#endif //IMATERIASOURCE_HPP
