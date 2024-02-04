ded12d44744d84244ba16610fdeb4f73bfa93742
snowhp <13867481+snowhp@users.noreply.github.com>
2024-02-04 17:33:51 +0000

    Fix: WrongCat and WrongAnimal now implement their on makeSound function for further testing

--- CPP04/ex00/WrongCat.cpp
+++ CPP04/ex00/WrongCat.cpp
@@ -1,26 +1,28 @@
 // Created by tde-sous on 21-01-2024.
 #include "WrongCat.hpp"
 
-
-
 WrongCat::WrongCat() {
-    std::cout << "[WrongCat] Default constructor called.\n";
-    this->type = "Miauu";
+  std::cout << "[WrongCat] Default constructor called.\n";
+  this->type = "Miauu";
 }
 
-WrongCat::WrongCat(const WrongCat& other)  : WrongAnimal(other) {
-    std::cout << "[WrongCat] Copy constructor called.\n";
-    this->type = other.type;
+WrongCat::WrongCat(const WrongCat &other) : WrongAnimal(other) {
+  std::cout << "[WrongCat] Copy constructor called.\n";
+  this->type = other.type;
 }
 
-WrongCat& WrongCat::operator=(const WrongCat& other) {
-    std::cout << "[WrongCat] Copy assignment operator called.\n";
-    if (this == &other)
-        return *this;
-    this->type = other.type;
-    return  *this;
+WrongCat &WrongCat::operator=(const WrongCat &other) {
+  std::cout << "[WrongCat] Copy assignment operator called.\n";
+  if (this == &other)
+    return *this;
+  this->type = other.type;
+  return *this;
 }
 
 WrongCat::~WrongCat() {
-    std::cout << "[WrongCat] Default destructor called.\n";
+  std::cout << "[WrongCat] Default destructor called.\n";
+}
+
+void WrongCat::makeSound() {
+  std::cout << "I am wrong miau miau!" << std::endl;
 }
--- CPP04/ex00/WrongCat.hpp
+++ CPP04/ex00/WrongCat.hpp
@@ -6,27 +6,26 @@
 
 class WrongCat : public WrongAnimal {
 public:
-   /**
+  /**
    * @brief Default constructor.
    */
-    WrongCat();
-   /**
+  WrongCat();
+  /**
    * @brief Copy constructor.
    * @param other The other WrongCat to copy.
    */
-    WrongCat(const WrongCat& other);
-   /**
+  WrongCat(const WrongCat &other);
+  /**
    * @brief Copy assignment operator.
    * @param other The other WrongCat to assign.
    * @return A reference to the assigned WrongCat.
    */
-    WrongCat& operator=(const WrongCat& other);
-   /**
+  WrongCat &operator=(const WrongCat &other);
+  /**
    * @brief Destructor.
    */
-    ~WrongCat();
+  ~WrongCat();
+  void makeSound();
 };
 
-
-
-#endif //WRONGCAT_HPP
+#endif // WRONGCAT_HPP
