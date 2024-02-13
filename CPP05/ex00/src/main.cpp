c966c7ccf17bd3bf2f72766189935c6deef9b190
snowhp <13867481+snowhp@users.noreply.github.com>
2024-02-13 12:18:34 +0000

    Tests: Using a Bureaucrat with a correct grade and performing some operations with it, without causing exceptions

--- CPP05/ex00/src/main.cpp
+++ CPP05/ex00/src/main.cpp
@@ -3,13 +3,14 @@
 #include "Bureaucrat.hpp"
 
 int main() {
-  //Initiate with a wrong grade
+  // Initiate with a wrong grade
   try {
     Bureaucrat BurA("other", 151);
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
   }
   std::cout << "============" << std::endl;
+
   // Initiate with a correct grade and perform wrong decrement
   try {
     Bureaucrat BurB("other", 150);
@@ -18,12 +19,28 @@ int main() {
     std::cout << e.what() << std::endl;
   }
   std::cout << "============" << std::endl;
+
   // Initiate with a correct grade and perform wrong increment
   try {
-    Bureaucrat BurB("other", 1);
-    BurB.decrementGrade();
-    BurB.incrementGrade();
-    BurB.incrementGrade();
+    Bureaucrat BurC("other", 1);
+    BurC.decrementGrade();
+    BurC.incrementGrade();
+    BurC.incrementGrade();
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+  std::cout << "============" << std::endl;
+
+  // Working Bureautcrat
+  try {
+    Bureaucrat BurD("Mister BurBur", 1);
+    std::cout << BurD << std::endl;
+    BurD.decrementGrade();
+    std::cout << BurD << std::endl;
+    BurD.decrementGrade();
+    std::cout << BurD << std::endl;
+    BurD.incrementGrade();
+    std::cout << BurD << std::endl;
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
   }
