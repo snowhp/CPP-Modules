7fc5c4c02de10fced4e41500060c9eac20731322
snowhp <13867481+snowhp@users.noreply.github.com>
2024-02-13 18:07:40 +0000

    Tests: Main function

--- CPP05/ex01/src/main.cpp
+++ CPP05/ex01/src/main.cpp
@@ -1,50 +1,27 @@
 // Created by tde-sous on 07-02-2024.
 
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
 int main() {
-  // Initiate with a wrong grade
+  // Expected behavior
   try {
-    Bureaucrat BurA("other", 151);
+    Bureaucrat BurA("other", 124);
+    Form FormA("Formâge A", 124,  123);
+    std::cout << FormA << std::endl;
+    FormA.beSigned(BurA);
+    std::cout << FormA << std::endl;
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
   }
   std::cout << "============" << std::endl;
 
-  // Initiate with a correct grade and perform wrong decrement
+  // Wrong Grade of FormA
   try {
-    Bureaucrat BurB("other", 150);
-    BurB.decrementGrade();
+    Form FormA("Formâge A", 151,  123);
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
   }
   std::cout << "============" << std::endl;
 
-  // Initiate with a correct grade and perform wrong increment
-  try {
-    Bureaucrat BurC("other", 1);
-    BurC.decrementGrade();
-    BurC.incrementGrade();
-    BurC.incrementGrade();
-  } catch (std::exception &e) {
-    std::cout << e.what() << std::endl;
-  }
-  std::cout << "============" << std::endl;
-
-  // Working Bureautcrat
-  try {
-    Bureaucrat BurD("Mister BurBur", 1);
-    std::cout << BurD << std::endl;
-    BurD.decrementGrade();
-    std::cout << BurD << std::endl;
-    BurD.decrementGrade();
-    std::cout << BurD << std::endl;
-    BurD.incrementGrade();
-    std::cout << BurD << std::endl;
-
-    Bureaucrat BurF(BurD);
-    std::cout << "I am a copy of BurD: " << BurF << std::endl;
-  } catch (std::exception &e) {
-    std::cout << e.what() << std::endl;
-  }
 }
