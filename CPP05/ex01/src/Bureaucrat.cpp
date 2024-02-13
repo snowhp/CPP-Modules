54d18f281f4a6eb7fc748e986d43717d5738cb52
snowhp <13867481+snowhp@users.noreply.github.com>
2024-02-13 16:52:50 +0000

    Add: Base files from previous exercise

---
+++ CPP05/ex01/src/Bureaucrat.cpp
@@ -0,0 +1,63 @@
+// Created by tde-sous on 07-02-2024.
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : name_("Unamed Bureaucrat"), grade_(150) {
+  std::cout << "[Bureaucrat] Default constructor called." << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string const &name, int const &grade)
+    : name_(name) {
+  std::cout << "[Bureaucrat] Constructor called." << std::endl;
+  if (grade < MAX_GRADE)
+    throw Bureaucrat::GradeTooHighException();
+  if (grade > MIN_GRADE)
+    throw Bureaucrat::GradeTooLowException();
+  this->grade_ = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : name_(other.name_), grade_() {
+  std::cout << "[Bureaucrat] Copy constructor called." << std::endl;
+  *this = other;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other) {
+  std::cout << "[Bureaucrat] Copy assignment called." << std::endl;
+  if (this == &other)
+    return *this;
+  this->grade_ = other.grade_;
+  return *this;
+}
+
+Bureaucrat::~Bureaucrat() {
+  std::cout << "[Bureaucrat] Destructor called." << std::endl;
+}
+
+void Bureaucrat::decrementGrade() {
+  if (this->grade_ + 1 > MIN_GRADE)
+    throw GradeTooLowException();
+  this->grade_++;
+}
+
+void Bureaucrat::incrementGrade() {
+  if (this->grade_ - 1 < MAX_GRADE)
+    throw GradeTooHighException();
+  this->grade_--;
+}
+
+unsigned int Bureaucrat::getGrade() const { return this->grade_; }
+
+std::string Bureaucrat::getName() const { return this->name_; }
+
+const char *Bureaucrat::GradeTooHighException::what() const throw() {
+  return ("Grade is too High!");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw() {
+  return ("Grade is too Low!");
+}
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &obj) {
+  os << obj.getName() << ", bureaucrat grade " << obj.getGrade() << "."
+     << std::endl;
+  return os;
+}
---
+++ CPP05/ex01/src/main.cpp
@@ -0,0 +1,50 @@
+// Created by tde-sous on 07-02-2024.
+
+#include "Bureaucrat.hpp"
+
+int main() {
+  // Initiate with a wrong grade
+  try {
+    Bureaucrat BurA("other", 151);
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+  std::cout << "============" << std::endl;
+
+  // Initiate with a correct grade and perform wrong decrement
+  try {
+    Bureaucrat BurB("other", 150);
+    BurB.decrementGrade();
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+  std::cout << "============" << std::endl;
+
+  // Initiate with a correct grade and perform wrong increment
+  try {
+    Bureaucrat BurC("other", 1);
+    BurC.decrementGrade();
+    BurC.incrementGrade();
+    BurC.incrementGrade();
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+  std::cout << "============" << std::endl;
+
+  // Working Bureautcrat
+  try {
+    Bureaucrat BurD("Mister BurBur", 1);
+    std::cout << BurD << std::endl;
+    BurD.decrementGrade();
+    std::cout << BurD << std::endl;
+    BurD.decrementGrade();
+    std::cout << BurD << std::endl;
+    BurD.incrementGrade();
+    std::cout << BurD << std::endl;
+
+    Bureaucrat BurF(BurD);
+    std::cout << "I am a copy of BurD: " << BurF << std::endl;
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+}
