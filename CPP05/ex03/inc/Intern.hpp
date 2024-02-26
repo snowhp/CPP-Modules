916abd87932ef84ab8d60524f950257589345600
snowhp <13867481+snowhp@users.noreply.github.com>
2024-02-26 18:19:06 +0000

    Add: Make Form of different concrete classes

--- CPP05/ex03/inc/Intern.hpp
+++ CPP05/ex03/inc/Intern.hpp
@@ -3,31 +3,65 @@
 #define INTERN_HPP
 
 #include "AForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 #include <iostream>
 
 class Intern {
 public:
-   /**
+  /**
    * @brief Default constructor.
    */
-    Intern();
-   /**
+  Intern();
+  /**
    * @brief Copy constructor.
    * @param other The other Intern to copy.
    */
-    Intern(const Intern& other);
-   /**
+  Intern(const Intern &other);
+  /**
    * @brief Copy assignment operator.
    * @param other The other Intern to assign.
    * @return A reference to the assigned Intern.
    */
-    Intern& operator=(const Intern& other);
-   /**
+  Intern &operator=(const Intern &other);
+  /**
    * @brief Destructor.
    */
-    ~Intern();
+  ~Intern();
+  /**
+   * @brief Creates a pointer to a Form Object and initialize the target to the
+   * second parameter.
+   * @param formName the FormName
+   * @param formTargetName the Form Target Name
+   * @return  pointer to a Form object (whose name is the one passed as
+   * parameter) whose target will be initialized to the second parameter
+   */
+  AForm *makeForm(const std::string &formName,
+                  const std::string &formTargetName);
+  /**
+   * \brief Creates a new residentialPardonForm
+   * \param target The name of the PresidentialPardonForm
+   * \return A new instance of type AForm
+   */
+  static AForm *makePresidentialPardonForm(const std::string &target);
+  /**
+   * \brief Creates a new RobotomyRequestForm
+   * \param target The name of the RobotomyRequestForm
+   * \return A new instance of type AForm
+   */
+  static AForm *makeRobotomyRequestForm(const std::string &target);
+  /**
+   * \brief Creates a new ShrubberyCreationForm
+   * \param target The name of the ShrubberyCreationForm
+   * \return A new instance of type AForm
+   */
+  static AForm *makeShrubberyCreationForm(const std::string &target);
+  class FormNotFound : public std::exception {
+  public:
+   /** @brief Throws an exception indicating that the Form is not Found. */
+    virtual const char *what() const throw();
+  };
 };
 
-
-
-#endif //INTERN_HPP
+#endif // INTERN_HPP
--- CPP05/ex03/src/Intern.cpp
+++ CPP05/ex03/src/Intern.cpp
@@ -2,21 +2,41 @@
 #include "Intern.hpp"
 
 
-
 Intern::Intern() {
     std::cout << "[Intern] Default constructor called." << std::endl;
 }
 
 Intern::Intern(const Intern& other) {
     std::cout << "[Intern] Copy constructor called." << std::endl;
+	(void) other;
 }
 
 Intern& Intern::operator=(const Intern& other) {
     std::cout << "[Intern] Copy assigment operator called." << std::endl;
+	(void) other;
     return *this;
 }
 
 Intern::~Intern() {
     std::cout << "[Intern] Destructor called." << std::endl;
 
+}
+
+AForm *Intern::makePresidentialPardonForm(const std::string &target)
+{
+	return (new PresidentialPardonForm(target));
+}
+
+AForm *Intern::makeRobotomyRequestForm(const std::string &target)
+{
+	return (new RobotomyRequestForm(target));
+}
+
+AForm *Intern::makeShrubberyCreationForm(const std::string &target)
+{
+	return (new ShrubberyCreationForm(target));
+}
+
+const char *Intern::FormNotFound::what() const throw() {
+	return "Form not found.";
 }
