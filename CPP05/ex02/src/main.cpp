fdc48360010aa1b67cf79f3ff22f14f51cba9d30
snowhp <13867481+snowhp@users.noreply.github.com>
2024-02-26 11:48:57 +0000

    Tests: ShrubberyCreationForm on main.cpp

--- CPP05/ex02/src/main.cpp
+++ CPP05/ex02/src/main.cpp
@@ -1,14 +1,18 @@
 // Created by tde-sous on 07-02-2024.
 
 #include "Bureaucrat.hpp"
-#include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 int main() {
-  // // 
-  // try {
-  //   
-  // } catch (std::exception &e) {
-  //   std::cout << e.what() << std::endl;
-  // }
+  // Expected behavior
+  try {
+    Bureaucrat BurA("other", 146);
+    ShrubberyCreationForm FormA("Formâge A");
+    std::cout << FormA << std::endl;
+    FormA.beSigned(BurA);
+    std::cout << FormA << std::endl;
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
   std::cout << "============" << std::endl;
 }
