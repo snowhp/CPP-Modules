6f2ce56cd07874d14d92ce96b3facfad7dfc026f
snowhp <13867481+snowhp@users.noreply.github.com>
2024-02-26 13:19:36 +0000

    Add: Canonical Form for PresidentialPardonForm Class and Constructor

--- CPP05/ex02/inc/PresidentialPardonForm.hpp
+++ CPP05/ex02/inc/PresidentialPardonForm.hpp
@@ -2,31 +2,39 @@
 #ifndef PRESIDENTIALPARDONFORM_HPP
 #define PRESIDENTIALPARDONFORM_HPP
 
+#include "AForm.hpp"
 
+class AForm;
 
-class PresidentialPardonForm {
+class PresidentialPardonForm : public AForm {
+private:
+  std::string target_;
+
 public:
-   /**
+  /**
    * @brief Default constructor.
    */
-    PresidentialPardonForm();
-   /**
+  PresidentialPardonForm();
+  /**
+   * @brief Constructor with target name.
+   * @param target The name of the target.
+   */
+  explicit PresidentialPardonForm(const std::string &target);
+  /**
    * @brief Copy constructor.
    * @param other The other PresidentialPardonForm to copy.
    */
-    PresidentialPardonForm(const PresidentialPardonForm& other);
-   /**
+  PresidentialPardonForm(const PresidentialPardonForm &other);
+  /**
    * @brief Copy assignment operator.
    * @param other The other PresidentialPardonForm to assign.
    * @return A reference to the assigned PresidentialPardonForm.
    */
-    PresidentialPardonForm& operator=(const PresidentialPardonForm& other);
-   /**
+  PresidentialPardonForm &operator=(const PresidentialPardonForm &other);
+  /**
    * @brief Destructor.
    */
-    ~PresidentialPardonForm();
+  ~PresidentialPardonForm();
 };
 
-
-
-#endif //PRESIDENTIALPARDONFORM_HPP
+#endif // PRESIDENTIALPARDONFORM_HPP
--- CPP05/ex02/src/PresidentialPardonForm.cpp
+++ CPP05/ex02/src/PresidentialPardonForm.cpp
@@ -1,21 +1,34 @@
 // Created by tde-sous on 26-02-2024.
 #include "PresidentialPardonForm.hpp"
 
-
-
-PresidentialPardonForm::PresidentialPardonForm() {
-    
+PresidentialPardonForm::PresidentialPardonForm()
+    : AForm("", 25, 5), target_("") {
+  std::cout << "[PresidentialPardonForm] Default constructor called."
+            << std::endl;
 }
 
-PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& other) {
-    
+PresidentialPardonForm::PresidentialPardonForm(const std::string &target)
+    : AForm(target, 25, 5), target_(target) {
+  std::cout << "[PresidentialPardonForm] Constructor called." << std::endl;
+}
+PresidentialPardonForm::PresidentialPardonForm(
+    const PresidentialPardonForm &other)
+    : AForm(other.target_, 25, 5), target_(other.target_) {
+  std::cout << "[PresidentialPardonForm] Copy constructor called." << std::endl;
+  this->setSigned(other.getSigned());
 }
 
-PresidentialPardonForm& PresidentialPardonForm::operator=(const PresidentialPardonForm& other) {
-    
+PresidentialPardonForm &
+PresidentialPardonForm::operator=(const PresidentialPardonForm &other) {
+  std::cout << "[PresidentialPardonForm] Copy assigment operator called."
+            << std::endl;
+  if (this == &other)
     return *this;
+  this->setSigned(other.getSigned());
+  this->target_ = other.target_;
+  return *this;
 }
 
 PresidentialPardonForm::~PresidentialPardonForm() {
-    
+  std::cout << "[PresidentialPardonForm] Destructor called." << std::endl;
 }
