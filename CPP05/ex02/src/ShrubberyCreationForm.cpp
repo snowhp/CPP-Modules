3f733e45471cfa804a35aac7cb0b098872802169
snowhp <13867481+snowhp@users.noreply.github.com>
2024-02-26 11:47:49 +0000

    Fix: std::endl used on messages

--- CPP05/ex02/src/ShrubberyCreationForm.cpp
+++ CPP05/ex02/src/ShrubberyCreationForm.cpp
@@ -2,32 +2,31 @@
 #include "ShrubberyCreationForm.hpp"
 
 ShrubberyCreationForm::ShrubberyCreationForm() : AForm("", 145, 137) {
-  std::cout << "[ShrubberyCreationForm] Default constructor called.";
+  std::cout << "[ShrubberyCreationForm] Default constructor called." << std::endl;
   this->asciiTreeWriter("");
 }
 
 ShrubberyCreationForm::ShrubberyCreationForm(const std::string &target)
     : AForm(target, 145, 137) {
-  std::cout << "[ShrubberyCreationForm] Default constructor called.";
+  std::cout << "[ShrubberyCreationForm] Default constructor called." << std::endl;
   this->asciiTreeWriter(target);
 }
 
 ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &other)
     : AForm(other.getName(), other.getSignGrade(), other.getExecGrade()) {
-  std::cout << "[ShrubberyCreationForm] Copy constructor called.";
+  std::cout << "[ShrubberyCreationForm] Copy constructor called." << std::endl;
   this->asciiTreeWriter(other.getName());
 }
 
 ShrubberyCreationForm &
-ShrubberyCreationForm::operator=(const ShrubberyCreationForm &other)
-    : AForm(other.getName(), other.getSignGrade(), other.getExecGrade()) {
-  std::cout << "[ShrubberyCreationForm] Copy assignment operator called.";
+ShrubberyCreationForm::operator=(const ShrubberyCreationForm &other) {
+  std::cout << "[ShrubberyCreationForm] Copy assignment operator called." << std::endl;
   this->asciiTreeWriter(other.getName());
   return *this;
 }
 
 ShrubberyCreationForm::~ShrubberyCreationForm() {
-  std::cout << "[ShrubberyCreationForm] Destructor called.";
+  std::cout << "[ShrubberyCreationForm] Destructor called." << std::endl;
 }
 
 void ShrubberyCreationForm::asciiTreeWriter(const std::string &name) {
