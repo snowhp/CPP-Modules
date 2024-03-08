9f09bb7eda361b1859c47573217e12185f7e8ae2
snowhp <13867481+snowhp@users.noreply.github.com>
2024-03-08 11:38:13 +0000

    Tests: Subject tests added commented

--- CPP07/ex00/src/main.cpp
+++ CPP07/ex00/src/main.cpp
@@ -4,28 +4,43 @@
 #include <iostream>
 
 int main() {
-  whatever obj;
   int a = 651;
   int b = 6;
   std::cout << "BEFORE SWAP: a = " << a << " b = " << b << std::endl;
-  obj.swap<int>(a, b);
+  ::swap<int>(a, b);
   std::cout << "AFTER SWAP: a = " << a << " b = " << b << std::endl;
 
   std::cout << "The min value between a(" << a << ") and b(" << b << ") is "
-            << obj.min<int>(a, b) << std::endl;
+            << ::min<int>(a, b) << std::endl;
 
   std::cout << "The max value between a(" << a << ") and b(" << b << ") is "
-            << obj.max<int>(a, b) << std::endl;
+            << ::max<int>(a, b) << std::endl;
 
   float c = 60.6123123;
   float d = 6.01;
   std::cout << "BEFORE SWAP: c = " << c << " d = " << d << std::endl;
-  obj.swap<float>(c, d);
+  ::swap<float>(c, d);
   std::cout << "AFTER SWAP: c = " << c << " d = " << d << std::endl;
 
   std::cout << "The min value between c(" << c << ") and d(" << d << ") is "
-            << obj.min<float>(c, d) << std::endl;
+            << ::min<float>(c, d) << std::endl;
 
   std::cout << "The max value between c(" << c << ") and d(" << d << ") is "
-            << obj.max<float>(c, d) << std::endl;
+            << ::max<float>(c, d) << std::endl;
 }
+
+// int main( void ) {
+//   int a = 2;
+//   int b = 3;
+//   ::swap( a, b );
+//   std::cout << "a = " << a << ", b = " << b << std::endl;
+//   std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+//   std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+//   std::string c = "chaine1";
+//   std::string d = "chaine2";
+//   ::swap(c, d);
+//   std::cout << "c = " << c << ", d = " << d << std::endl;
+//   std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+//   std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+//   return 0;
+// }
