46d1336a0a977885b9abdc803c0ccef580c9027a
snowhp <13867481+snowhp@users.noreply.github.com>
2024-03-08 17:24:54 +0000

    Add: Main.cpp and Makefile To CPP07/ex02

---
+++ CPP07/ex02/src/main.cpp
@@ -0,0 +1,31 @@
+// Created by tde-sous on 08-03-2024.
+
+#include "Array.hpp"
+#include <iostream>
+
+int main() {
+  try {
+    char letter = 'a';
+    Array<char> obj(10);
+    for (int i = 0; i <= 10; i++) {
+      obj[i] = letter++;
+      std::cout << "Index " << i << " = " << obj[i] << std::endl;
+    }
+    obj[8] = 'a';
+    std::cout << obj[8] << std::endl;
+    std::cout << "Size of the object is " << obj.size() << std::endl;
+  } catch (const std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  try {
+    const int size = 10;
+    Array<int> obj(size);
+    for (int i = 0; i < size; i++) {
+      obj[i] = i;
+      std::cout << "Index " << i << " = " << obj[i] << std::endl;
+    }
+  } catch (const std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+}
