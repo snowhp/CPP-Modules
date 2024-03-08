53abb1a1f2311b4c306792d2cc6223ea3741a5b8
snowhp <13867481+snowhp@users.noreply.github.com>
2024-03-08 11:38:28 +0000

    Fix: Remove the class and only defined the template member functions

--- CPP07/ex00/inc/whatever.hpp
+++ CPP07/ex00/inc/whatever.hpp
@@ -2,26 +2,20 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
-
-
-class whatever {
-public:
-  template <typename T>
-  void swap(T &num1, T &num2) {
-      T tmp = num1;
-      num1 =  num2;
-      num2 = tmp;
-  };
-  template <typename T>
-    T min(T &num1, T &num2) {
-     return (num1 < num2 ? num1 : num2);
-  };
-  template <typename T>
-    T max(T &num1, T &num2) {
-    return (num1 > num2 ? num1 : num2);
-  };
+template <typename T>
+void swap(T &num1, T &num2) {
+    T tmp = num1;
+    num1 =  num2;
+    num2 = tmp;
 };
-
+template <typename T>
+  T min(T &num1, T &num2) {
+   return (num1 < num2 ? num1 : num2);
+};
+template <typename T>
+  T max(T &num1, T &num2) {
+  return (num1 > num2 ? num1 : num2);
+};
 
 
 #endif //WHATEVER_HPP
