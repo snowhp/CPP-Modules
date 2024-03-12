049c943a1bef39dfc68e45d975818954fb1d83b9
snowhp <13867481+snowhp@users.noreply.github.com>
2024-03-12 18:56:33 +0000

    Add: Main calls for convert

--- CPP06/ex00/src/main.cpp
+++ CPP06/ex00/src/main.cpp
@@ -1,5 +1,10 @@
 // Created by tde-sous on 28-02-2024.
 
-int main() {
+#include "ScalarConverter.hpp"
 
+int main(int argc, char **argv) {
+  if (argc != 2)
+    return(std::cout << "Only send 1 argument!" << std::endl, 1);
+  ScalarConverter::convert(argv[1]);
+  return (0);
 }
