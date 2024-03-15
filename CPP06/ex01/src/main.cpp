cb0d6e4e722f14dca43143dd6cb9dd2a2ee712b2
snowhp <13867481+snowhp@users.noreply.github.com>
2024-03-15 14:04:55 +0000

    Tests: main.cpp for Serializer class

--- CPP06/ex01/src/main.cpp
+++ CPP06/ex01/src/main.cpp
@@ -1,9 +1,35 @@
 // Created by tde-sous on 15-03-2024.
 
 #include "Serializer.hpp"
-#include "Data.hpp"
+#include <iostream>
 
-int main()
-{
+int main() {
+  Data GameSerializer = {10, 100, 42, false, false};
+  std::cout << "Before serialize()" << std::endl
+            << "Level: " << GameSerializer.level
+            << " Health: " << GameSerializer.health
+            << " Points: " << GameSerializer.points
+            << " GameCompleted?: " << GameSerializer.GameComplete
+            << " BossDefeated?: " << GameSerializer.BossDefeated << std::endl;
 
+  uintptr_t converted = Serializer::serialize(&GameSerializer);
+  std::cout << "After serialize()" << std::endl
+            << "Level: " << *reinterpret_cast<int *>(converted)
+            << " Health: " << *reinterpret_cast<int *>(converted + sizeof(int))
+            << " Points: "
+            << *reinterpret_cast<int *>(converted + 2 * sizeof(int))
+            << " GameCompleted?: "
+            << *reinterpret_cast<bool *>(converted + 3 * sizeof(int))
+            << " BossDefeated?: "
+            << *reinterpret_cast<bool *>(converted + 3 * sizeof(int) +
+                                         sizeof(bool))
+            << std::endl;
+
+  Data *reconverted = Serializer::deserialize(converted);
+  std::cout << "After deserialize()" << std::endl
+            << "Level: " << reconverted->level
+            << " Health: " << reconverted->health
+            << " Points: " << reconverted->points
+            << " GameCompleted?: " << reconverted->GameComplete
+            << " BossDefeated?: " << reconverted->BossDefeated << std::endl;
 }
