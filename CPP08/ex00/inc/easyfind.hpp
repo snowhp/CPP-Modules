ae77dba91402faeba4a2852f02dc1147cfe7746d
snowhp <13867481+snowhp@users.noreply.github.com>
2024-03-21 13:50:30 +0000

    Add: Base files for CPP08

---
+++ CPP08/ex00/inc/easyfind.hpp
@@ -0,0 +1,10 @@
+//
+// Created by tde-sous on 3/20/24.
+//
+
+#ifndef EX00_EASYFIND_HPP
+#define EX00_EASYFIND_HPP
+
+
+#include "easyfind.tpp"
+#endif // EX00_EASYFIND_HPP
---
+++ CPP08/ex00/src/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by tde-sous on 3/20/24.
+//
+
+#include "easyfind.hpp"
+
+int main()
+{
+}
