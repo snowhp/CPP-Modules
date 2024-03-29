7b82806d69640189749eb94d7034c7985ec0cf43
snowhp <13867481+snowhp@users.noreply.github.com>
2024-03-29 11:35:39 +0000

    Tests: Span addNumber and exceptions

--- CPP08/ex01/src/main.cpp
+++ CPP08/ex01/src/main.cpp
@@ -5,4 +5,15 @@
 #include "Span.hpp"
 
 int main() {
+  Span obj(5);
+  try {
+    obj.addNumber(1);
+    obj.addNumber(2);
+    obj.addNumber(3);
+    obj.addNumber(4);
+    obj.addNumber(5);
+    obj.addNumber(5);
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
 }
