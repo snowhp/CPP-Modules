14d5564e48667a44c2b5cb0e3b972c71d6f276e1
snowhp <13867481+snowhp@users.noreply.github.com>
2024-03-29 12:46:04 +0000

    Tests: Main from subject

--- CPP08/ex01/src/main.cpp
+++ CPP08/ex01/src/main.cpp
@@ -4,16 +4,27 @@
 
 #include "Span.hpp"
 
-int main() {
+/*int main() {
   Span obj(5);
   try {
-    obj.addNumber(-1);
-    obj.addNumber(-1);
-    obj.addNumber(-1);
-    obj.addNumber(-1);
-    obj.addNumber(-1);
+    obj.addNumber(111);
+    obj.addNumber(119);
     std::cout << "Longest Span is = " << obj.longestSpan() << std::endl;
+    std::cout << "Shortest Span is = " << obj.shortestSpan() << std::endl;
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
   }
+}*/
+
+int main()
+{
+  Span sp = Span(5);
+  sp.addNumber(6);
+  sp.addNumber(3);
+  sp.addNumber(17);
+  sp.addNumber(9);
+  sp.addNumber(11);
+  std::cout << sp.shortestSpan() << std::endl;
+  std::cout << sp.longestSpan() << std::endl;
+  return 0;
 }
