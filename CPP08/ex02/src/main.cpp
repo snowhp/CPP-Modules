c0b8e4b2c03e0b680006be200932f83e1902248d
snowhp <13867481+snowhp@users.noreply.github.com>
2024-03-29 17:29:17 +0000

    Tests: Own tests added

--- CPP08/ex02/src/main.cpp
+++ CPP08/ex02/src/main.cpp
@@ -3,8 +3,8 @@
 //
 
 #include "MutantStack.hpp"
-#include <iterator>
 
+/*
 int main()
 {
   MutantStack<int> mstack;
@@ -16,6 +16,7 @@ int main()
   mstack.push(3);
   mstack.push(5);
   mstack.push(737);
+  //[...]
   mstack.push(0);
   MutantStack<int>::iterator it = mstack.begin();
   MutantStack<int>::iterator ite = mstack.end();
@@ -28,4 +29,29 @@ int main()
   }
   std::stack<int> s(mstack);
   return 0;
+}*/
+
+int main()
+{
+  MutantStack<char> cstack;
+  cstack.push('a');
+  cstack.push('a');
+  std::cout << cstack.top() << std::endl;
+  std::cout << cstack.size() << std::endl;
+
+  std::cout << "Alphabet test" << std::endl;
+  MutantStack<char> dstack;
+  for(char c = 'a'; c <= 'z'; c++)
+    dstack.push(c);
+  std::cout << "Size of dstack is " << dstack.size() << std::endl;
+  std::cout << "Top of dstack is " << dstack.top() << std::endl;
+
+  MutantStack<char>::iterator it_a = dstack.begin();
+  MutantStack<char>::iterator it_b = dstack.end();
+
+  while(it_a != it_b)
+  {
+    std::cout << *it_a << std::endl;
+    it_a++;
+  }
 }
