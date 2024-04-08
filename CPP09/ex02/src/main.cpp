18373001c17b9706dd38a1888da272f83d7b74bd
snowhp <13867481+snowhp@users.noreply.github.com>
2024-04-08 18:33:04 +0100

    Refactor: Better readability

--- CPP09/ex02/src/main.cpp
+++ CPP09/ex02/src/main.cpp
@@ -7,17 +7,18 @@
 int main(int argc, char **argv) {
 
   if (argc == 1)
-    return (std::cout << "Please provide numbers to be sorted." << std::endl, 1);
+    return (std::cout << "Please provide numbers to be sorted." << std::endl,
+            1);
   for (int i = 1; i < argc; i++) {
     std::string a = argv[i];
-    if (a.find_first_not_of("0123456789") != std::string::npos)
-      return (std::cout << "Error: Invalid number provided " << argv[i]
-                        << std::endl,
-              1);
+    if (a.find_first_not_of("0123456789") != std::string::npos) {
+      std::cout << "Error: Invalid number provided " << argv[i] << std::endl;
+      return 1;
+    }
     long double num = std::strtol(argv[i], NULL, 10);
-    if (num == 0 || num > std::numeric_limits<int>::max())
-      return (std::cout << "Error: Invalid number provided " << argv[i]
-                        << std::endl,
-              1);
+    if (num == 0 || num > std::numeric_limits<int>::max()) {
+      std::cout << "Error: Invalid number provided " << argv[i] << std::endl;
+      return 1;
+    }
   }
 }
